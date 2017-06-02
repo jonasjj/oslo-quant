e181778fbb4bcbc10a33121dbbeda485ff92d436
Jonas Jensen <jojuljen@gmail.com>
2017-06-02 21:39:09 +0200

    statistics working for OMX. OBX gives 'nan'

--- python/historical_return_from_to_date.py
+++ python/historical_return_from_to_date.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 import argparse
 from pprint import pprint
+from collections import OrderedDict
+import statistics
 
 from markets import get_instrument
 
@@ -16,7 +18,7 @@ def parse_date(date_str):
     """
     return datetime.strptime(date_str, "%Y-%m-%d")
 
-def find_expected_return_from_to_date(instrument, buy_date, sell_date):
+def find_historical_return_from_to_date(instrument, buy_date, sell_date):
 
     if buy_date > sell_date:
         raise ValueError("Sell date is before buy date")
@@ -44,27 +46,68 @@ def find_expected_return_from_to_date(instrument, buy_date, sell_date):
     if _buy_date < first_timestamp or _sell_date > last_timestamp:
         raise KeyError("Not enough data")
     
-    years = []
+    trades = []
     accumulated_gain = 0
+    accumulated_gain_ratio = 0
+    trade_count = 0
+    pos_gain_trades = 0
+    neg_gain_trades = 0
+
+    gain_ratios = []
+        
     while _sell_date <= last_timestamp:
+        trade_count += 1
 
         buy = instrument.get_day_or_first_after(_buy_date)
         sell = instrument.get_day_or_first_after(_sell_date)
-        gain = sell ['value'] - buy['value']
-        years.append((buy['date'], sell['date'], buy['value'], sell['value'], gain))
+        gain = sell['value'] - buy['value']
+        gain_ratio = (sell['value'] / buy['value']) - 1
+        gain_ratios.append(gain_ratio)
+        accumulated_gain += gain
+        accumulated_gain_ratio += gain_ratio
+
+        if gain > 0:
+            pos_gain_trades += 1
+        
+        trade=OrderedDict()
+        trade['buy_date'] = buy['date']
+        trade['sell_date'] = sell['date']
+        trade['buy'] = buy['value']
+        trade['sell'] = sell['value']
+        trade['gain_ratio'] = gain_ratio
+        trades.append(trade)
 
         # increment by 1 year
         _buy_date = datetime(_buy_date.year + 1, _buy_date.month, _buy_date.day)
         _sell_date = datetime(_sell_date.year + 1, _sell_date.month, _sell_date.day)
 
-    return years
-    
-    
+    if trade_count == 0:
+        raise ValueError("There are no historical trades for these dates")
+        
+    avg_gain = accumulated_gain / trade_count
+    avg_gain_ratio = accumulated_gain_ratio / trade_count
 
+    try:
+        pos_gain_ratio = pos_gain_trades / trade_count
+    except ZeroDivisionError:
+        pos_gain_ratio = 0
+        
+    variance = statistics.variance(gain_ratios)
+    std_deviation = statistics.stdev(gain_ratios)
+        
+    data = OrderedDict()
+    data['trades'] = trades
+    data['trade_count'] = trade_count
+    data['avg_gain_ratio'] = avg_gain_ratio
+    data['pos_gain_ratio'] = pos_gain_ratio
+    data['variance'] = variance
+    data['std_deviation'] = std_deviation
+    return data
+                      
 if __name__ == "__main__":
 
     # Parse command line arguments
-    parser = argparse.ArgumentParser(description="Find the expected return between dates")
+    parser = argparse.ArgumentParser(description="Find the historical return between dates")
     parser.add_argument("instrument", help="Instrument name (ex.: OBX)")
     parser.add_argument("buy_date", help="Buy date")
     parser.add_argument("sell_date", help="Sell date")
@@ -76,4 +119,4 @@ if __name__ == "__main__":
     buy_date = parse_date(args.buy_date)
     sell_date = parse_date(args.sell_date)
 
-    pprint(find_expected_return_from_to_date(instrument, buy_date, sell_date))
+    pprint(find_historical_return_from_to_date(instrument, buy_date, sell_date))
