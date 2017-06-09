56cbb0fecdca06aefa56e485c3d76e5dcbfe2a9f
Jonas Jensen <jojuljen@gmail.com>
2017-06-09 22:40:16 +0200

    Changed return type to avoid recomputation

--- python/historical_return_best_dates.py
+++ python/historical_return_best_dates.py
@@ -5,8 +5,7 @@ from tabulate import tabulate
 from markets import get_instrument
 from historical_return_from_to_date import historical_return_from_to_date
 
-def _historical_return_dates(instrument,
-                             days_between):
+def historical_return_dates(instrument, days_between):
     """"
     Find the historically best dates to buy and sell
     
@@ -15,8 +14,13 @@ def _historical_return_dates(instrument,
         days_between (int): Approximate number of days between buy and sell
 
     Return:
-        unsorted list of tuples where:
-            [(buy_date, avg_gain_ratio, pos_gain_ratio),..]
+        dict with two lists sorted on average gain ratio
+        and change of positive gain ratio:
+
+            {'avg_gain_ratio': 
+                [(buy_date, sell_date, avg_gain_ratio, pos_gain_ratio),..],
+             'pos_gain':
+                [(buy_date, sell_date, avg_gain_ratio, pos_gain_ratio),..]}
     """
     year = 2017
     date = datetime(year, 1, 1)
@@ -37,27 +41,14 @@ def _historical_return_dates(instrument,
         
         date += timedelta(days=1)
 
-    return results
-
-def historical_return_best_dates_by_avg_gain_ratio(instrument,
-                                                   days_between):
+    # sort by column a, then by column b
+    avg_gain_list = sorted(results, key=lambda x: (x[2], x[3]), reverse=True)
+    pos_gain_list = sorted(results, key=lambda x: (x[3], x[2]), reverse=True)
     
-    results = _historical_return_dates(instrument, days_between)
-
-    # sort first by column 2, then by column 3
-    results.sort(key=lambda x: (x[2], x[3]), reverse=True)
-                 
-    return results
-
-def historical_return_best_dates_by_pos_gain_ratio(instrument,
-                                                   days_between):
+    d = {'avg_gain_ratio': avg_gain_list,
+         'pos_gain_ratio': pos_gain_list}
     
-    results = _historical_return_dates(instrument, days_between)
-
-    # sort first by column 3, then by column 2
-    results.sort(key=lambda x: (x[3], x[2]), reverse=True)
-    
-    return results
+    return d
 
 def _print(results):
     """Print a tabulated list"""
@@ -101,26 +92,28 @@ if __name__ == "__main__":
     
     # get the instrument from the database
     instrument = get_instrument(args.instrument.upper())
+
+    # run the algorithm
+    res = historical_return_dates(instrument, args.days_between)
     
     if args.avg_gain:
-        res = historical_return_best_dates_by_avg_gain_ratio(instrument,
-                                                             args.days_between)
+        l = res['avg_gain_ratio']
+        
         if args.worst:
-            res.reverse()
+            l.reverse()
             print("\nWorst dates sorted by average gain")
         else:
             print("\nBest dates sorted by average gain")
             
-        _print(res[:args.topn -1])
+        _print(l[:args.topn -1])
             
     if args.pos_gain:
-        res = historical_return_best_dates_by_pos_gain_ratio(instrument,
-                                                             args.days_between)
+        l = res['pos_gain_ratio']
         
         if args.worst:
-            res.reverse()
+            l.reverse()
             print("\nWorst dates sorted by change of positive gain gain")
         else:
             print("\nBest dates sorted by change of positive gain gain")
             
-        _print(res[:args.topn -1])
+        _print(l[:args.topn -1])
