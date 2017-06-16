46de2dd9b17dd5ebcd52e0d8be280edcc7b61ed8
Jonas Jensen <jojuljen@gmail.com>
2017-06-16 23:42:03 +0200

    Implemented plotting of ticker/column

--- python/plot_instrument.py
+++ python/plot_instrument.py
@@ -1,11 +1,27 @@
 import argparse
 
-def plot():
-    from markets import get_instrument
-    from plotting import linked_plot
-    
-    linked_plot([(get_instrument("OBX").data, 'close', 'OBX'),
-                 (get_instrument("OMXO20GI").data, 'value', 'omxo20gi'),])
+from markets import get_instrument
+from plotting import linked_plot
+
+def plot(ticker_columns):
+    """
+    Args:
+       ticker_columns (list): Tickers and ticker data columns to plot.
+          This argument must be an iterable on the format:
+             [[instrument_name_a, column_name_a],
+              [instrument_name_b, column_name_b],
+              ...]          
+    """
+
+    # create linked_plot() arguments
+    l = []
+    for instrument_name, column_name in ticker_columns:
+        instrument = get_instrument(instrument_name.upper())
+        plot_title = instrument_name.upper() + "_" + column_name
+        l.append((instrument.data, column_name, plot_title))
+
+    # create the plot
+    linked_plot(l)
 
 if __name__ == '__main__':
 
@@ -13,7 +29,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Plot instruments")
 
     parser.add_argument('-i', metavar=("TICKER", "COLUMN"), nargs=2, action='append',
-                        help="Plot column ('open', 'value', etc.) from instrument")
+                        help="Plot column ('open', 'value', etc.) from ticker")
     args = parser.parse_args()
-    print(args)
+
+    plot(args.i)
+    
     
