e3870b495b64c189d393142620100750208a480f
Jonas Jensen <jojuljen@gmail.com>
2017-10-28 23:23:03 +0200

    Created test case for upcoming linked plot changes

---
+++ python/tests/plottest.py
@@ -0,0 +1,22 @@
+import numpy as np
+import math
+
+import sys
+sys.path.append('..')
+
+from plotting import LinkedPlot
+
+x_axis = tuple(float(x) for x in range(100))
+y1_axis = tuple(math.sin(x) for x in x_axis)
+y2_axis = tuple(math.cos(x) for x in x_axis)
+
+matrix = np.array(list(zip(x_axis, y1_axis, y2_axis)),
+                  dtype=[('x', 'f8'),
+                         ('y1', 'f8'),
+                         ('y2', 'f8')])
+
+linked_plot = LinkedPlot(window_title="LOL Window")
+linked_plot.add_plot(plot_title="LOL Plot")
+linked_plot.add_subplot(matrix, x_axis_name='x', y_axis_name='y1')
+linked_plot.add_subplot(matrix, x_axis_name='x', y_axis_name='y2')
+linked_plot.show()
