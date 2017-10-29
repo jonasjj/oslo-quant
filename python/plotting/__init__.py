c559b98d66361548cf22d27356fee60cf65f423e
Jonas Jensen <jojuljen@gmail.com>
2017-10-29 23:15:35 +0100

    Changed calling scheme of the plotting module. Instead of passing everything through the linked_plot() function to the LinkedPlotWidget's init, the caller should import the new LinkedPlot class.  By using LinkedPlot's add_plot(), add_subplot(), and show() functions linked plots with and without subplots can be created.

--- python/plotting/__init__.py
+++ python/plotting/__init__.py
@@ -1,1 +1,1 @@
-from plotting._linked_plot import linked_plot
+from plotting._linked_plot import LinkedPlot
--- python/tests/plottest.py
+++ python/tests/plottest.py
@@ -11,12 +11,14 @@ y1_axis = tuple(math.sin(x) for x in x_axis)
 y2_axis = tuple(math.cos(x) for x in x_axis)
 
 matrix = np.array(list(zip(x_axis, y1_axis, y2_axis)),
-                  dtype=[('x', 'f8'),
+                  dtype=[('date', 'f8'),
                          ('y1', 'f8'),
                          ('y2', 'f8')])
 
 linked_plot = LinkedPlot(window_title="LOL Window")
-linked_plot.add_plot(plot_title="LOL Plot")
-linked_plot.add_subplot(matrix, x_axis_name='x', y_axis_name='y1')
-linked_plot.add_subplot(matrix, x_axis_name='x', y_axis_name='y2')
+linked_plot.add_plot(plot_title="LOL Plot1")
+linked_plot.add_subplot(matrix, y_axis_name='y1')
+linked_plot.add_subplot(matrix, y_axis_name='y2')
+linked_plot.add_plot(plot_title="LOL Plot2")
+linked_plot.add_subplot(matrix, y_axis_name='y1')
 linked_plot.show()
