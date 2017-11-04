b9a9293c3e696e974a57c5c29ff0dd49378e3f84
Jonas Jensen <jojuljen@gmail.com>
2017-11-04 13:10:43 +0100

    Function for getting top performing stocks over a time period

--- python/strategy/_momentumstrategy.py
+++ python/strategy/_momentumstrategy.py
@@ -31,10 +31,44 @@ class MomentumStrategy(Strategy):
         # the date to compare against
         compare_date = today - self.compare_delta
 
+        top_performers = self.get_top_performers(compare_date,
+                                                 self.number_of_stocks)
+        return []
+
+    def get_top_performers(self, buy_date, count):
+        """
+        Get the N top performing stocks within a date interval
+
+        args:
+            buy_date(datetime.date): Buy date. Sell date is today
+            count(int): Number of top performers to return
+
+        return:
+            The N top performing stocks
+        """
+        sell_date = self.today
+
         # let's focus on stocks noted on Oslo Børs (that exist at from_date)
         instruments = self.get_instruments(oslobors=True, nasdaqomx=False)
 
-        # keep only instruments that exist today, and existed at the compare date
-        instruments = [x for x in instruments if x.existed_at_date(compare_date)]
+        # keep only instruments that exist today and at the compare date
+        instruments = [
+            x for x in instruments if x.existed_at_date(buy_date)]
 
-        return []
+        # list of tuples (gain_ratio, instrument)
+        l = []
+
+        # construct the list
+        for i in instruments:
+            buy = i.get_price(buy_date)
+            sell = i.get_price(sell_date)
+            gain_ratio = sell / buy
+            l.append((gain_ratio, i))
+
+        # sort the list based on gain_ratio
+        l.sort(key=lambda x: x[0], reverse=True)
+
+        # keep only the instrument objects
+        top_performers = [x[1] for x in l[:count]]
+
+        return top_performers
